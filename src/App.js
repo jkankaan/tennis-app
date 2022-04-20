57d2c5a7b9840bb3423cbe832db3940526449fa7
Juhani Kankaanpää <juhanikankaanpaa@Juhanis-MBP.home>
2022-04-20 19:01:26 +0300

    ranking added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+/* eslint-disable react/jsx-no-undef */
 // eslint-disable-next-line no-unused-vars
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import Form from './components/Form.js';
@@ -12,12 +13,20 @@ function App() {
   const [player, setPlayer] = useState('');
   // eslint-disable-next-line no-unused-vars
   const [matches, setMatches] = useState('');
+  const [rankings, setRankings] = useState('');
+  const [ranking, setRanking] = useState('');
 
   function addPlayer(name) {
     const player = {name};
     console.log(player);
     setPlayer(name);
+    var rank = rankings.filter((playerIndex) => playerIndex.PARTICIPANT_NAME === name);
+    console.log(rank);
+    console.log(rank[0].RANK);
+    setRanking(rank[0].RANK);
+    console.log('RANKING', ranking);
   }
+    
 
   const today = new Intl.DateTimeFormat('fr-CA', {year: 'numeric',
     month: '2-digit', day: '2-digit'}).format(Date.now());
@@ -36,25 +45,48 @@ function App() {
   function findNested(obj, key, value) {
     // Base case
     if (obj[key] === value) {
+      console.log('_1_');
       return obj;
     } else {
-      for (var i = 0, len = Object.keys(obj).length; i < len; i++) {
-        if (typeof obj[i] == 'object') {
-          var found = this.findNested(obj[i], key, value);
+      console.log('_2_');
+      var keys = Object.keys(obj);
+      for (var i = 0, len = keys.length; i < len; i++) {
+        var k = keys[i];
+        console.log(obj[i]);
+        console.log({len});
+        console.log('_3_');
+        if (obj[k] && typeof obj[k] == 'object') {
+          console.log('_4_');
+          console.log('k:', k);
+          var found = findNested(obj[k], key, value);
           if (found) {
             // If the object was found in the recursive call, bubble it up.
-            return found;
+            return found;/* */
           }
         }
       }
     }
   }
 
+  function listRankings(props) {
+    const rankings = props;
+    const topTen = rankings.slice(0,10);
+    console.log('ranking',{rankings});
+    if (topTen.length > 0) {
+      var playerList = topTen.map((playerIndex) => 
+        <li key={playerIndex.RANK}>
+          {playerIndex.PARTICIPANT_NAME} </li>
+      );
+      return (
+        <ol> {playerList}  </ol>
+      );
+    }
+  }
+
   useEffect(() => {
     function fetchData() {
       axios({
-        url: 'http://localhost:3000/profile',
-        params: { answer: 42 },
+        url: 'http://localhost:3000/matches',
         method: 'get',
         timeout: 8000,
       })
@@ -65,7 +97,26 @@ function App() {
         .catch(err => console.error(err));
     }
     fetchData();
+    function fetchRanking() {
+      const options = {
+        method: 'GET',
+        url: 'https://flashscore.p.rapidapi.com/v1/rankings/data',
+        params: {locale: 'fi_FI', ranking_id: 'dSJr14Y8'},
+        headers: {
+          'X-RapidAPI-Host': 'flashscore.p.rapidapi.com',
+          'X-RapidAPI-Key': 'a7689aa85bmsh3c9e073ec9511aep1b03b9jsnf18f2bfa47d0'
+        }
+      };
+      axios(options)
+        .then(res => {
+          res.data.DATA;
+          setRankings(res.data.DATA);
+        })
+        .catch(err => console.error(err));
+    }
+    fetchRanking();
   },[]);
+
   
     
   /*function findMatch() {
@@ -81,10 +132,19 @@ function App() {
     // eslint-disable-next-line no-unused-vars
     tournaments.findIndex((isPlayer, index) =>  {console.log('löytyi');});
   } */
-
-  //const match = findNested(matches, 'lastname', 'Ruusuvuori');
-  //console.log({match});
-  //console.log('2');
+  console.log(typeof(matches));
+  const osuma = findNested(matches, 'name', 'Ruusuvuori');
+  console.log({matches});
+  console.log({osuma});
+  const obj = Object.values(matches);
+  console.log(Object.values(matches));
+  console.log(obj[1]);
+  var keysbyindex = Object.keys(matches);
+  console.log({keysbyindex});
+  for (var i=0; i<keysbyindex.length; i++)
+    alert(matches[keysbyindex[i]]);
+  console.log({rankings});
+  
 
   /* findMatch();*/
 
@@ -95,7 +155,7 @@ function App() {
         
         <Form addPlayer={addPlayer}/>
 
-        {/*{findNested(matches, 'lastname', 'Ruusuvuori' )}*/}
+        {/*findNested(matches, 'lastname', 'Ruusuvuori' )*/}
         
         <a
           className="App-link"
@@ -103,7 +163,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {player}
+          {ranking} {player}
+        </a>
+        <a>
+          {listRankings(rankings)}
         </a>
       </header>
     </div>
